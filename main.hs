1f5c94865eaee9983d68e0cec58a706385d38e1a
Bianca Sabrina Sabrina <84732687+biancasbublitz@users.noreply.github.com>
2021-06-12 20:47:34 +0000

    tentativa de implementação - com erro

--- main.hs
+++ main.hs
@@ -3,10 +3,55 @@
 
 import Text.Printf
 
+leLarg :: IO(Int)
+leLarg = do 
+ putStr "Digite a largura da imagem:  "
+ readLn
+
+leAlt :: IO(Int)
+leAlt = do 
+ putStr "Digite a altura da imagem:  "
+ readLn
+
+lePonto :: IO(Int,Int)
+lePonto = do 
+ putStr "Digite um ponto inicial. Escreva nesse modelo (larg, alt) :  "
+ readLn
+
+-- String inicial do SVG
+svgInicio :: Int -> Int -> String
+svgInicio w h = printf "<svg width='%.2f' height='%.2f' xmlns='http://www.w3.org/2000/svg'>\n" w h 
+
+-- String final do SVG
+svgFinal :: String
+svgFinal = "</svg>"
+
+svgCircle :: Int -> Int -> Int -> String -> String 
+svgCircle x y r style = 
+  printf "<circle cx='%d' cy='%d' r='%d' fill='%s' />\n" x y r style
+
+svgAll :: Int -> Int -> String
+svgAll x y = 
+  svgInicio x y ++ 
+  (svgCircle 10 10 50 "rgb(255, 255, 255,0.5)") ++ 
+  svgFinal
+
 main :: IO ()
 main = do
-putStrLn "Digite a largura da imagem"
-larg <- getLine
-putStrLn "Digite a altura da imagem"
-alt <- getLine
-putStrLn ("Voce digitou " ++ larg ++ " " ++ alt)
+  larg <- leLarg
+  alt <- leAlt
+  ponto <- lePonto
+  putStrLn "Voce digitou " 
+  print larg
+  writeFile "circles.svg" (svgAll larg alt)
+
+ {-putStrLn "Digite a largura da imagem"
+ larg <- getLine
+ putStrLn "Digite a altura da imagem"
+ alt <- getLine
+ putStrLn "Digite um ponto inicial. Escreva nesse modelo '(larg, alt)'"
+ ponto <- getLine
+ putStrLn ("Voce digitou " ++ larg ++ " " ++ alt ++ " " ++ ponto)-}
+ 
+        
+       

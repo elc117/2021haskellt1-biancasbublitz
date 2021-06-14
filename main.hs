b326b8bd84d2aa001d928d6ca00c7c09016804ac
Bianca Sabrina Sabrina <84732687+biancasbublitz@users.noreply.github.com>
2021-06-14 01:58:34 +0000

    implementação já com a saída de algumas figuras, mas em hardcode

--- main.hs
+++ main.hs
@@ -2,6 +2,7 @@
 -- Bianca Sabrina Bublitz
 
 import Text.Printf
+--import System.Random (randomRIO)
 
 leLarg :: IO(Int)
 leLarg = do 
@@ -18,22 +19,34 @@ lePonto = do
  putStr "Digite um ponto inicial. Escreva nesse modelo (larg, alt) :  "
  readLn
 
+leRaio :: IO(Int)
+leRaio = do 
+ putStr "Digite o raio dos circulos:  "
+ readLn
+
+corAleat :: String
+corAleat = "rgb(255, 255, 255, 1)"
+
+criaCirc :: Int -> Int -> Int -> Int -> Int -> String
+criaCirc larg alt x y raio = (svgCircle x y raio corAleat)
+
 -- String inicial do SVG
 svgInicio :: Int -> Int -> String
-svgInicio w h = printf "<svg width='%.2f' height='%.2f' xmlns='http://www.w3.org/2000/svg'>\n" w h 
+svgInicio w h = printf "<svg width='%d' height='%d' xmlns='http://www.w3.org/2000/svg'>\n" w h 
 
 -- String final do SVG
 svgFinal :: String
 svgFinal = "</svg>"
 
 svgCircle :: Int -> Int -> Int -> String -> String 
-svgCircle x y r style = 
-  printf "<circle cx='%d' cy='%d' r='%d' fill='%s' />\n" x y r style
+svgCircle x y r cor = 
+  printf "<circle cx='%d' cy='%d' r='%d' fill='none' stroke='%s' stroke-width='5' />\n" x y r cor
 
-svgAll :: Int -> Int -> String
-svgAll x y = 
-  svgInicio x y ++ 
-  (svgCircle 10 10 50 "rgb(255, 255, 255,0.5)") ++ 
+svgAll :: Int -> Int -> (Int,Int) -> Int -> String
+svgAll larg alt (x,y) raio = 
+  svgInicio larg alt ++ 
+  (criaCirc larg alt x y raio) ++ 
+  (criaCirc larg alt x y (raio*2)) ++ 
   svgFinal
 
 main :: IO ()
@@ -41,17 +54,5 @@ main = do
   larg <- leLarg
   alt <- leAlt
   ponto <- lePonto
-  putStrLn "Voce digitou " 
-  print larg
-  writeFile "circles.svg" (svgAll larg alt)
-
- {-putStrLn "Digite a largura da imagem"
- larg <- getLine
- putStrLn "Digite a altura da imagem"
- alt <- getLine
- putStrLn "Digite um ponto inicial. Escreva nesse modelo '(larg, alt)'"
- ponto <- getLine
- putStrLn ("Voce digitou " ++ larg ++ " " ++ alt ++ " " ++ ponto)-}
- 
-        
-       
+  raio <- leRaio
+  writeFile "circles.svg" (svgAll larg alt ponto raio)

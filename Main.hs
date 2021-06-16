f7ddfba8aa663d45ca505d51784872a9957db6b0
Bianca Sabrina Sabrina <84732687+biancasbublitz@users.noreply.github.com>
2021-06-16 01:37:50 +0000

    implementação de novas cores

--- Main.hs
+++ Main.hs
@@ -21,12 +21,6 @@ leAlt = do
  putStr "Digite a altura da imagem:  "
  readLn
 
--- Le ponto inicial dos circulos digitado pelo usuário
-lePonto :: IO(Int,Int)
-lePonto = do 
- putStr "Digite um ponto inicial. Escreva nesse modelo (larg, alt) :  "
- readLn
-
 -- Le raio dos circulos digitada pelo usuário
 leRaio :: IO(Int)
 leRaio = do 
@@ -43,9 +37,23 @@ menor x y = if x <= y then x else y
 -------------------------------------------------------------------------------
 
 -- Paleta com n valores retirados de uma lista com sequências de R, G e B 
-rgbPalette :: Int -> [(Int,Int,Int)]
---rgbPalette n = take n $ cycle [(255,90,90),(90,255,90),(90,90,255)]
-rgbPalette n = take n [(255,255,255)]
+rgbPalette :: Int -> Int -> [(Int,Int,Int)]
+rgbPalette 0 r = []
+rgbPalette n r 
+  | n <= 5 = (r^2, r , r) : rgbPalette (n-1) (r+1)
+  | n <= 10 = (r, r^2 , r) : rgbPalette (n-1) (r+2)
+  | n <= 15 = (r, r , r^2) : rgbPalette (n-1) (r+3)
+  | n <= 20 = (r, r , r) : rgbPalette (n-1) (r+4)
+  | n <= 30 = (r, r , r) : rgbPalette (n-1) (r+6)
+  | n <= 40 = (r, r , r) : rgbPalette (n-1) (r+8)
+  | otherwise = (r, r , r) : rgbPalette (n-1) (r+10)
+{-
+rgbPalette 20 r = (r^2, r , r^2) : rgbPalette (20-1) (r+2)
+rgbPalette n r = (r, r , r) : rgbPalette (n-1) (r+8)-}
+
+semVogais (x:xs)
+   | elem x "aeiou" = semVogais xs
+   | otherwise      = x : semVogais xs
 
 -------------------------------------------------------------------------------
 -- Geração de circulos em suas posições
@@ -68,7 +76,7 @@ genCircs ((x,y), r) quant inicial = ((x,y), r) : (genCircs ((x,y), r+inicial) (q
 -- dadas coordenadas e dimensões do circulo e uma string com atributos de estilo
 svgCirc :: Circle -> String -> String 
 svgCirc ((x,y), r) style = 
-  printf "<circle cx='%d' cy='%d' r='%d' fill='none' stroke='%s' stroke-width='5' />\n" x y r style
+  printf "<circle cx='%d' cy='%d' r='%d' fill='none' stroke='%s' stroke-width='3' />\n" x y r style
 
 -- String inicial do SVG
 svgInicial :: Int -> Int -> String
@@ -84,9 +92,10 @@ svgStyle (r,g,b) = printf "rgb(%d,%d,%d)" r g b
 
 -- Gera strings SVG para uma dada lista de figuras e seus atributos de estilo
 -- Recebe uma função geradora de strings SVG, uma lista de círculos e strings de estilo
-svgElements :: (a -> String -> String) -> [a] -> [String] -> Int -> [String]
-svgElements func [] styles quant = []
-svgElements func (x:xs) styles quant = concat $ zipWith func [x] styles : [svgElements func xs styles (quant-1)]
+svgElements :: (a -> String -> String) -> [a] -> [String] -> [String]
+svgElements func [] styles = []
+svgElements func circ [] = []
+svgElements func (x:xs) (y:ys) = concat $ zipWith func [x] [y] : [svgElements func xs ys]
 
 -------------------------------------------------------------------------------
 -- Função principal que gera arquivo com imagem SVG
@@ -96,13 +105,13 @@ main :: IO ()
 main = do
   larg <- leLarg
   alt <- leAlt
-  ponto <- lePonto
   raio <- leRaio
+  let ponto = ((div larg 2),(div alt 2))
   let menorNum = menor alt larg
   let numCircs = div (div menorNum raio) 2
-      palette = rgbPalette numCircs
+      palette = rgbPalette numCircs raio
       circs = genCircs (geraUmCirc ponto raio) numCircs raio
-      svgfigs = svgElements svgCirc circs (map svgStyle palette) numCircs
+      svgfigs = svgElements svgCirc circs (map svgStyle palette)
       svgstrs = concat ([svgInicial larg alt] ++ (svgfigs) ++ [svgFinal])
    in writeFile "img.svg" svgstrs
   
